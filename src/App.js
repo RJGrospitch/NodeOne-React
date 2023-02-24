12f7bd80f1db81049411d2ded05cee42adf5e16a
BlockChainGaming <RobertGrospitch@Gmail.Com>
2023-02-24 11:02:20 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {Amplify} from 'aws-amplify';
+import awsExports from "./aws-exports";
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Auth } from "aws-amplify";
+
+// Configure Amplify in index file or root file
+Amplify.configure({
+  Auth: {
+    region: awsExports.REGION,
+    userPoolId: awsExports.USER_POOL_ID,
+    userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Authenticator>
+        {({ signOut, user }) => (
+            <div>
+              <p>Welcome {user.username}</p>
+              <button onClick={signOut}>Sign out</button>
+            </div>
+        )}
+      </Authenticator>
   );
 }
 
---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+// replace the user pool region, id, and app client id details
+export default {
+    "REGION": "us-east-1",
+    "USER_POOL_ID": "us-east-1_1LaJbil63",
+    "USER_POOL_APP_CLIENT_ID": "ii5prtcqe8cqav431tk25l695"
+}
+
+
+
